ba19e6ff3437d8a905712ecdbd12bf091d27aecb
hoseok <shsshs0125@naver.com>
2023-03-14 11:44:23 +0900

    Profile Count Info Done

---
+++ lib/components/profile_buttons.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class ProfileButtons extends StatelessWidget {
+  const ProfileButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_profile_practice/components/profile_count_info.dart';
+import 'package:flutter_profile_practice/components/profile_header.dart';
 import 'package:flutter_profile_practice/theme.dart';
 
 import 'components/profile_drawer.dart';
@@ -31,57 +33,15 @@ class ProfilePage extends StatelessWidget {
       appBar: _appBar(),
       body: Column(
         children: [
-          Row(
-            children: [
-              SizedBox(width: 20),
-              _buildHeaderAvatar(),
-              SizedBox(width: 20),
-              _buildHeaderProfile()
-            ],
-          ),
-          Row(),
-          Row(),
+          SizedBox(height: 20),
+          ProfileHeader(),
+          SizedBox(height: 20),
+          ProfileCountInfo(),
+          SizedBox(height: 20),
           ProfileTab(),
         ],
       ),
     );
-  }
-
-  Column _buildHeaderProfile() {
-    return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "GetinThere",
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                Text(
-                  "프로그래머/작가/강사",
-                  style: TextStyle(
-                    fontSize: 20,
-                  ),
-                ),
-                Text(
-                  "데어 프로그래밍",
-                  style: TextStyle(
-                    fontSize: 15,
-                  ),
-                ),
-              ],
-            );
-  }
-
-  SizedBox _buildHeaderAvatar() {
-    return SizedBox(
-              width: 100,
-              height: 100,
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/avatar.png"),
-              ),
-            );
   }
 }
 
